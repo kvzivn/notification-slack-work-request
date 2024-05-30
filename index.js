f25aec64a1eab2f057987f20231e4490b9345179
Kevin <hello@kevinivan.com>
2024-05-30 13:24:12 +0200

    put inside try catch

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ export default async ({ req, res, log, error }) => {
   const { stakeholderId, goal } = JSON.parse(req.body)
   // const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
 
-  const client = new Client()
-  client
-    .setEndpoint("https://appwrite.teamspark.xyz/v1")
-    .setProject("spark-net-app")
-    .setKey(process.env.APPWRITE_API_KEY)
-
-  const databases = new Databases(client)
-
-  const stakeholder = await databases.getDocument(
-    process.env.APPWRITE_DATABASE_ID,
-    process.env.APPWRITE_STAKEHOLDER_COLLECTION_ID,
-    stakeholderId
-  )
-
-  const { name, email } = stakeholder.document
-
-  const message = `We have a new work request!\n- Name: ${name}\n- Email: ${email}${
-    goal ? `\n- Goal: ${goal}` : ""
-  }`
-
   try {
+    const client = new Client()
+    client
+      .setEndpoint("https://appwrite.teamspark.xyz/v1")
+      .setProject("spark-net-app")
+      .setKey(process.env.APPWRITE_API_KEY)
+
+    const databases = new Databases(client)
+
+    const stakeholder = await databases.getDocument(
+      process.env.APPWRITE_DATABASE_ID,
+      process.env.APPWRITE_STAKEHOLDER_COLLECTION_ID,
+      stakeholderId
+    )
+
+    const { name, email } = stakeholder.document
+
+    const message = `We have a new work request!\n- Name: ${name}\n- Email: ${email}${
+      goal ? `\n- Goal: ${goal}` : ""
+    }`
+
     log(message)
     // await axios.post(slackWebhookUrl, {
     //   text: message,
